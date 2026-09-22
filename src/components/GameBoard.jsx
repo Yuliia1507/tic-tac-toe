08288c2f036f0f87e8f64b29045ad8d190e3cd9b
Yuliia <yulia.zakhovaiko@gmail.com>
2026-09-22 19:47:18 +0300

    disabled buttons conditionally

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -26,7 +26,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
 								<button
 									onClick={() =>
 										onSelectSquare(rowIndex, colIndex)
-									}
+									} disabled={playerSymbol !== null}
 								>
 									{playerSymbol}
 								</button>
